013950ff74fa0339f71b2bcf3d46b7601dc0cffd
Abhishekgitok <abhishek19781@gmail.com>
2024-05-06 18:22:19 +0530

    api update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8080/api/v1/category/get-category");
+      const { data } = await axios.get("https://e-commerce-api-hu0x.onrender.com/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -67,7 +67,7 @@ const HomePage = () => {
   const loadMore = async () => {
     try {
       setLoading(true);
-      const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+      const { data } = await axios.get(`https://e-commerce-api-hu0x.onrender.com/api/v1/product/product-list/${page}`);
       setLoading(false);
       setProducts([...products, ...data?.products]);
     } catch (error) {
@@ -97,7 +97,7 @@ const HomePage = () => {
   //get filterd product
   const filterProduct = async () => {
     try {
-      const { data } = await axios.post("/api/v1/product/product-filters", {
+      const { data } = await axios.post("https://e-commerce-api-hu0x.onrender.com/api/v1/product/product-filters", {
         checked,
         radio,
       });
